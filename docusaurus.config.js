30ceaaf4187940951ff910bbe7a10860b494fe42
lab-zhejiang <githubzj@zhejianglab.org>
2024-12-12 17:30:40 +0800

    Update docusaurus.config.js
    Signed-off-by: lab-zhejiang <githubzj@zhejianglab.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Zhejiang Lab',
-  tagline: '之江实验室',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
