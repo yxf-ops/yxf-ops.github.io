91c8daf30e486834a23927724c9a57b4e46fc463
lab-zhejiang <githubzj@zhejianglab.org>
2024-12-13 10:08:44 +0800

    Update docusaurus.config.js
    Signed-off-by: lab-zhejiang <githubzj@zhejianglab.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,6 @@ const config = {
       image: 'img/byconity-social-card.png',
       navbar: {
         title: 'Lab',
-        hideOnScroll: true,
         logo: {
           alt: 'Zhejiang Lab Logo',
           src: 'img/logo.png',
