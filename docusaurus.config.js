359e9f7fdc43d249cff7054a6ec9f0b1f1511d2e
lab-zhejiang <githubzj@zhejianglab.org>
2024-12-16 16:25:18 +0800

    Update docusaurus.config.js
    Signed-off-by: lab-zhejiang <githubzj@zhejianglab.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -201,7 +201,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} ByteDance.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ZheJiang Lab.`, 
       },
       prism: {
         theme: lightCodeTheme,
